6f0c40bed6be4afb04dcdfb2a8d49e26c5e46294
yangdemin <yangdemin1990@163.com>
2021-07-19 23:06:09 +0800

    v3 -> v5

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from configparser import ConfigParser,ExtendedInterpolation
 import websocket
 import hmac
 import base64
-import zlib
-
+import os
+import traceback
+import errno
 #log config
 import logging
+import logging.handlers
 from datetime import datetime
 
 symbol1LastCallTime  = 0.0 
@@ -19,63 +21,49 @@ symbol2LastCallTime  = 0.0
 symbol3LastCallTime  = 0.0 
 symbol4LastCallTime  = 0.0 
 
+def logInitNew():
+    try:
+        os.makedirs("./logs", exist_ok=True)  # Python>3.2
+    except TypeError:
+        try:
+            os.makedirs("./logs")
+        except OSError as exc:  # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir("./logs"):
+                pass
+            else:
+                raise
 
-url = 'wss://real.okex.com:10442/ws/v3'
+
+    logger = logging.getLogger(__file__)
+    logger.setLevel(logging.INFO)
+    #logger =  logging.getLogger("asyncio").setLevel(logging.INFO)
+
+    logHandler = logging.handlers.TimedRotatingFileHandler(
+        filename="./logs/log", when='D', interval=1, backupCount=3, encoding='utf-8')
+
+    LOG_FORMAT1 = "%(asctime)s- %(levelname)s - %(message)s [%(filename)s:%(lineno)s]"
+    #LOG_FORMAT1 = "%(message)s [%(filename)s:%(lineno)s]"
+    logFormatter = logging.Formatter(LOG_FORMAT1)
+    logHandler.setFormatter(logFormatter)
+
+    if not logger.handlers:
+        streamhandler = logging.StreamHandler()
+        streamhandler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(levelname)s- %(message)s')
+        streamhandler.setFormatter(formatter)
+        logger.addHandler(streamhandler)
+        logger.addHandler(logHandler)
+
+    return logger
+
+log = logInitNew()
+url = 'wss://ws.okex.com:8443/ws/v5/public'
 
 class cfgSet:
     pass
 
 cfgSet = cfgSet()
 
-def loginParams(ws):
-    unixTime = time.time()
-    timestamp = str(round(unixTime))
-    message = timestamp + 'GET' + '/users/self/verify'
-    mac = hmac.new(bytes(cfgSet.seceret_key, encoding='utf8'), bytes(message, encoding='utf-8'), digestmod='sha256')
-    d = mac.digest()
-    sign = base64.b64encode(d)
-    login_param = {"op": "login", "args": [cfgSet.api_key, cfgSet.passphrase, timestamp, sign.decode("utf-8")]}
-    login_str = json.dumps(login_param)
-    ws.send(login_str)
-
-def on_message(ws, message):  # 服务器有数据更新时，主动推送过来的数据
-    decompress = zlib.decompressobj(-zlib.MAX_WBITS) # see above
-    inflated = decompress.decompress(message)
-    inflated += decompress.flush()
-    data = str(inflated,encoding='utf-8')
-    ticker = json.loads(data)
-    logging.info(ticker)
-    if 'data' not in ticker:
-        print(ticker)
-        return
-    num = len(ticker['data'])
-    i = 0
-    try: 
-        while i < num:
-            dataCheck(ticker['data'][i]['instrument_id'],ticker['data'][0])
-            #logging.info(ticker['data'][i]['timestamp'])
-            i = i+1
-    except Exception as e:
-        print("error ",str(e))
-        logging.error(str(e))
-
-def on_error(ws, error):  # 程序报错时，就会触发on_error事件
-    print("on_error",str(error))
-    logging.error("on_error",str(error))
-
-def on_close(ws):
-    logging.info("Connection closed ……")
-    print("Connection closed ……")
-    time.sleep(60)
-    logging.info("Connection restart!")
-    webSocketRun()
-
-def on_open(ws):  # 连接到服务器之后就会触发on_open事件，这里用于send数据
-    loginParams(ws)
-    time.sleep(2)
-    sub_param = {"op": "subscribe", "args":cfgSet.subscribe}
-    sub_str = json.dumps(sub_param)
-    ws.send(sub_str)
 
 # init
 def quoteWatchInit():
@@ -86,49 +74,84 @@ def quoteWatchInit():
         cfg.read('./config.ini')
         cfg.sections()
 
-        cfgSet.api_key = str(cfg.get('Unity','api_key'))
-        cfgSet.seceret_key = str(cfg.get('Unity','seceret_key'))
-        cfgSet.passphrase = str(cfg.get('Unity','passphrase'))
-        print(cfgSet.api_key+","+cfgSet.seceret_key+","+cfgSet.passphrase)
-        logging.info(cfgSet.api_key+","+cfgSet.seceret_key+","+cfgSet.passphrase)
+        #cfgSet.api_key = str(cfg.get('Unity','api_key'))
+        #cfgSet.seceret_key = str(cfg.get('Unity','seceret_key'))
+        #cfgSet.passphrase = str(cfg.get('Unity','passphrase'))
+        #log.info(cfgSet.api_key+","+cfgSet.seceret_key+","+cfgSet.passphrase)
         cfgSet.phoneKey = str(cfg.get('Unity','phoneKey'))
-        print(cfgSet.phoneKey)
-        logging.info(cfgSet.phoneKey)
+        log.info(cfgSet.phoneKey)
 
         cfgSet.phone = cfg.get('Unity','phone')
-        channel = cfg.get('Unity','subscribe')
-        cfgSet.subscribe = channel.split(',')
-        logging.info(cfgSet.subscribe)
-        print(cfgSet.subscribe)
-        logging.info(str(cfgSet.phone))
+        log.info(str(cfgSet.phone))
 
         cfgSet.symbol1 = str(cfg.get('pair1','symbol'))
         cfgSet.priceHigh1 = cfg.getfloat('pair1','priceHigh')
         cfgSet.priceLow1 = cfg.getfloat('pair1','priceLow')
-        logging.info("pair1: "+str(cfgSet.symbol1)+" "+str(cfgSet.priceHigh1)+" "+str(cfgSet.priceLow1))
+        log.info("pair1: "+str(cfgSet.symbol1)+" "+str(cfgSet.priceHigh1)+" "+str(cfgSet.priceLow1))
         print(("pair1: "+str(cfgSet.symbol1)+" "+str(cfgSet.priceHigh1)+" "+str(cfgSet.priceLow1)))
 
         cfgSet.symbol2 = str(cfg.get('pair2','symbol'))
         cfgSet.priceHigh2 = cfg.getfloat('pair2','priceHigh')
         cfgSet.priceLow2 = cfg.getfloat('pair2','priceLow')
-        logging.info("pair2: "+str(cfgSet.symbol2)+" "+str(cfgSet.priceHigh2)+" "+str(cfgSet.priceLow2 ))
-        print("pair2: "+str(cfgSet.symbol2)+" "+str(cfgSet.priceHigh2)+" "+str(cfgSet.priceLow2 ))
-
+        log.info("pair2: "+str(cfgSet.symbol2)+" "+str(cfgSet.priceHigh2)+" "+str(cfgSet.priceLow2 ))
+       
         cfgSet.symbol3 = str(cfg.get('pair3','symbol'))
         cfgSet.priceHigh3 = cfg.getfloat('pair3','priceHigh')
         cfgSet.priceLow3 = cfg.getfloat('pair3','priceLow')
-        logging.info("pair3: "+str(cfgSet.symbol3)+" "+str(cfgSet.priceHigh3)+" "+str(cfgSet.priceLow3))
-        print("pair3: "+str(cfgSet.symbol3)+" "+str(cfgSet.priceHigh3)+" "+str(cfgSet.priceLow3))
-
+        log.info("pair3: "+str(cfgSet.symbol3)+" "+str(cfgSet.priceHigh3)+" "+str(cfgSet.priceLow3))
+       
         cfgSet.symbol4 = str(cfg.get('pair4','symbol'))
         cfgSet.priceHigh4 = cfg.getfloat('pair4','priceHigh')
         cfgSet.priceLow4 = cfg.getfloat('pair4','priceLow')
-
-        logging.info("pair4: "+str(cfgSet.symbol4)+" "+str(cfgSet.priceHigh4)+" "+str(cfgSet.priceLow4))
-        print("pair4: "+str(cfgSet.symbol4)+" "+str(cfgSet.priceHigh4)+" "+str(cfgSet.priceLow4))
+        log.info("pair4: "+str(cfgSet.symbol4)+" "+str(cfgSet.priceHigh4)+" "+str(cfgSet.priceLow4))
+       
     except Exception as e:
-        logging.error("config error ",str(e))
+        log.error("config error ",str(e))
         print("config error ",str(e))
+
+def on_message(ws, message):  # 服务器有数据更新时，主动推送过来的数据
+    info = json.loads(message)
+    if "event" in info:
+        if info["event"] == "error":
+            log.error(info)
+            return
+        elif info["event"] == "subscribe":
+            log.info(info)
+            return 
+
+    ticker = info
+    log.info(info)
+    if 'data' not in ticker:
+        log.info(info)
+        return
+
+    dataCheck(ticker['data'][0]['instId'],ticker['data'][0])
+    #log.info(ticker['data'][i]['timestamp'])
+
+def on_error(ws, error):  # 程序报错时，就会触发on_error事件
+    log.error("on_error",str(error))
+
+def on_close(ws):
+    log.info("Connection closed ……")
+    print("Connection closed ……")
+    time.sleep(60)
+    log.info("Connection restart!")
+    webSocketRun()
+
+def on_open(ws):  # 连接到服务器之后就会触发on_open事件，这里用于send数据
+    # loginParams(ws)
+    # time.sleep(2)
+    sub_param = {"op": "subscribe", 
+                 "args":[
+                     {"channel":"tickers","instId":cfgSet.symbol1},
+                     {"channel":"tickers","instId":cfgSet.symbol2},
+                     {"channel":"tickers","instId":cfgSet.symbol3},
+                     {"channel":"tickers","instId":cfgSet.symbol4}
+                    ]
+                }
+    sub_str = json.dumps(sub_param)
+    ws.send(sub_str)
+
 # check time ,check price 
 def dataCheck(symbol,data):
 
@@ -136,7 +159,7 @@ def dataCheck(symbol,data):
         lastPrice = float(data["last"])
     except Exception as e:
         print("error ",str(e))
-        logging.info("error ",str(e))
+        log.info("error ",str(e))
 
     # check price 
     if symbol == cfgSet.symbol1:
@@ -168,7 +191,7 @@ def dataCheck(symbol,data):
         else:
             return
     else:
-        logging.info("error symbol:",symbol)
+        log.info("error symbol:",symbol)
         print("error symbol:",symbol)
     
     #check time
@@ -218,23 +241,17 @@ def sendCall(warnInfo):
         timeout=10 , verify=False)
         result =  json.loads( resp.content )
     except Exception as e:
-        logging.error("config error ",str(e))
+        log.error("config error ",str(e))
         result = {
             'error': -99999
         }
         json_str = json.dumps(result)
         print(json_str)
     print("call "+cfgSet.phone+" :"+warnInfo)
-    logging.info("call "+cfgSet.phone+" :"+warnInfo)
-    logging.info(result)
+    log.info("call "+cfgSet.phone+" :"+warnInfo)
+    log.info(result)
     print(result)
     return result
-
-def logInit():
-    LOG_FORMAT = "%(asctime)s- %(levelname)s - %(message)s [%(filename)s:%(lineno)s]"
-    filename = datetime.now().strftime("%Y%m%d-%H%M%S")+'.log'
-    logging.basicConfig(filename="./logs/"+filename, level=logging.INFO, format=LOG_FORMAT)
-    print("create ./logs/",filename)
 
 def webSocketRun():
     ws = websocket.WebSocketApp(url,
@@ -243,11 +260,9 @@ def webSocketRun():
                                 on_close=on_close)
     ws.on_open = on_open
     ws.run_forever(ping_timeout=15)
-    logging.info("webSocketRun")
-    print("webSocket Runing ....")
+    log.info("webSocketRun ....")
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    logInit()
     quoteWatchInit()
     webSocketRun()
